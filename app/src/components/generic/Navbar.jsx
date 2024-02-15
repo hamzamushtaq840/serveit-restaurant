2da500f88a32c0bec5835b7d75dcb4c2f97abf27
Hamza Mushtaq <73603757+hamzamushtaq840@users.noreply.github.com>
2024-02-15 16:21:58 +0500

    feat: Add onClick event to navigate to cart page from cart icon div

--- app/src/components/generic/Navbar.jsx
+++ app/src/components/generic/Navbar.jsx
@@ -2,9 +2,11 @@ import React from "react"
 import { IoMenu } from "react-icons/io5"
 import help from "./../../assets/navbar/help.svg"
 import { IoCartOutline } from "react-icons/io5"
+import { useNavigate } from "react-router-dom"
 
 const Navbar = () => {
   let restaurentName = "Texas Grill House"
+  const navigate = useNavigate()
 
   return (
     <nav className="flex items-center justify-between px-4">
@@ -16,7 +18,11 @@ const Navbar = () => {
       </div>
       <div className="flex w-[60px] items-center gap-3">
         <img src={help} className="h-5 w-6" alt="" srcset="" />
-        <div className="relative">
+        <div
+          onClick={() => {
+            navigate("/cart")
+          }}
+          className="relative">
           <IoCartOutline className="h-6 w-6" />
           <div className="absolute -right-1 -top-1 flex h-4 w-4 items-center justify-center  rounded-full bg-primary text-xs text-white">
             3
